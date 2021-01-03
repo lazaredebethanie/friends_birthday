bc9348327718df9bac7ba6e2fda4dbdbc0205129
f5rpn <eric.fog@free.fr>
2021-01-03 18:38:03 +0100

    version 0

---
+++ app/src/main/java/fr/f5rpn/dbaccessv2/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.f5rpn.dbaccessv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
